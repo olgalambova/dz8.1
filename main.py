d34a79c1a0cb21f591bdd4c125ad708bfd35f207
olgalambova <lambova25@gmail.com>
2024-06-02 23:05:49 +0300

    dz8.1

---
+++ main.py
@@ -0,0 +1,22 @@
+lst_numbers = [1, 2, 3, 4]
+x = int(''.join(map(str, lst_numbers)))
+print(x)
+def add_one(x):
+    return x + 1
+print(add_one(x))
+int = add_one(x)
+stroka = str(int)
+print(stroka)
+ss = list(stroka)
+print(ss)
+
+
+
+
+#some_list = print(''.join(map(str, some_list)))
+
+#def add_one(some_list):
+    #return some_list.append('one')
+
+
+
